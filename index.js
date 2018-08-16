4502e933ca36cec56220b5f177b8b85ec2e73957
Brendan regan <brendanregan100@gmail.com>
2018-08-16 17:02:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+
+const driver = {
+  type: 'Ferrari',
+};
+
+// updateDriverWithKeyAndValue()
+ 
+// destructivelyUpdateDriverWithKeyAndValue()
+
+// deleteFromDriverByKey()
+
+// destructivelyDeleteFromDriverByKey()
